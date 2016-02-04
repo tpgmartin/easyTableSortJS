85b9a85094bc42901271cfea8e471597951817ae
Tom Martin <tom@tpgmartin.com>
2016-02-04 22:50:52 +0000

    Initial setup

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./src/easy_table_sort');
---
+++ src/easy_table_sort.js
@@ -0,0 +1,21 @@
+// TODO
+// 1. Find thead, table elements, table rows in DOM
+// 2. Find column index
+// 3. Get cell in table that corresponds to column
+// 4. Create array of cel values and row number
+// 5. Perform sort on this array
+// 6. Replace table rows with sorted array
+// 7. Remove class names from siblings
+// . Bind click event to table
+
+module.exports = function easyTableSort() {
+  
+  function dsfsdf (path) {
+    return;
+  }
+
+  return {
+    incrementMajor: incrementMajor
+  }
+  
+}
